8dc81ac7e7a62b95a916baf32be7f00cc3a14d79
MatiV14 <matias993@gmail.com>
2023-10-02 12:05:58 -0300

    .

--- Paquetes Turisticos/src/proyecto/transversal/entidades/Alojamiento.java
+++ Paquetes Turisticos/src/proyecto/transversal/entidades/Alojamiento.java
@@ -1,14 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto.transversal.entidades;
 
-/**
- *
- * @author MiPc
- */
+import java.time.LocalDate;
+
+
 public class Alojamiento {
+    
+    private int idAlojamiento;
+    private LocalDate fechaIn;
+    private LocalDate fechaOn;
+    private boolean estado;
+    private double importeDiario;
+    private Ciudad ciudadDest;
+
+    public Alojamiento(int idAlojamiento, LocalDate fechaIn, LocalDate fechaOn, boolean estado, double importeDiario, Ciudad ciudadDest) {
+        this.idAlojamiento = idAlojamiento;
+        this.fechaIn = fechaIn;
+        this.fechaOn = fechaOn;
+        this.estado = estado;
+        this.importeDiario = importeDiario;
+        this.ciudadDest = ciudadDest;
+    }
+
+    public Alojamiento() {
+    }
+
+    public int getIdAlojamiento() {
+        return idAlojamiento;
+    }
+
+    public void setIdAlojamiento(int idAlojamiento) {
+        this.idAlojamiento = idAlojamiento;
+    }
+
+    public LocalDate getFechaIn() {
+        return fechaIn;
+    }
+
+    public void setFechaIn(LocalDate fechaIn) {
+        this.fechaIn = fechaIn;
+    }
+
+    public LocalDate getFechaOn() {
+        return fechaOn;
+    }
+
+    public void setFechaOn(LocalDate fechaOn) {
+        this.fechaOn = fechaOn;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public double getImporteDiario() {
+        return importeDiario;
+    }
+
+    public void setImporteDiario(double importeDiario) {
+        this.importeDiario = importeDiario;
+    }
+
+    public Ciudad getCiudadDest() {
+        return ciudadDest;
+    }
+
+    public void setCiudadDest(Ciudad ciudadDest) {
+        this.ciudadDest = ciudadDest;
+    }
+    
+    
     
 }
