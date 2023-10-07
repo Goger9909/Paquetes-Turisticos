8fee4d8e7918dbf0d5b90bee6d954a3f6fdf9226
MatiV14 <matias993@gmail.com>
2023-10-07 07:37:31 -0300

    En proceso de prueba
    .

--- Paquetes Turisticos/src/proyecto/transversal/accesoADatos/Alojamiento_Data.java
+++ Paquetes Turisticos/src/proyecto/transversal/accesoADatos/Alojamiento_Data.java
@@ -18,18 +18,15 @@ import proyecto.transversal.entidades.Ciudad;
 public class Alojamiento_Data {
    
     Connection con = null;
-    Alojamiento_Data ald= new Alojamiento_Data();
-    Ciudad_Data cd= new Ciudad_Data();
     Alojamiento alojamiento = new Alojamiento();
     Ciudad ciudad = new Ciudad();
     
     
     public Alojamiento_Data(){
         con = Conexion.getConexion();
-        
-        
-        
+          
     }
+    
     
     public void guardarAlojamiento(Alojamiento alojamiento){
     
@@ -132,7 +129,7 @@ public class Alojamiento_Data {
             JOptionPane.showMessageDialog(null, "Error al acceder a la base de datos" + ex);
         }
     }
-    // BUSQUEDA POR ID DE ALOJAMIENTO - PUEDE QUE NECESITES BUSQUEDA DE ID DE LA CIUDAD PARA EL PAQUETE ASI QUE LOS DEJO MAS ABAJO  A ESOS METODOS JAJA
+    // BUSQUEDA POR ID DE ALOJAMIENTO - PUEDE QUE NECESITES BUSQUEDA de alojamiento PERO CON LA ID DE LA CIUDAD PARA EL PAQUETE, ASI QUE LOS DEJO MAS ABAJO  A ESOS METODOS JAJA
     public void buscarAlojamientoPorId(int id){
     
         String sql = "SELECT Fecha_Inicio, Fecha_Salida, Tipo_Alojamiento, Servicio, "
@@ -143,7 +140,7 @@ public class Alojamiento_Data {
             ps.setInt(1,id);
             ResultSet rs = ps.executeQuery();
             if(rs.next()){
-            
+                Ciudad_Data cd= new Ciudad_Data();
                 alojamiento.setIdAlojamiento(id);
                 alojamiento.setFechaIn(rs.getDate("Fecha_Inicio").toLocalDate());
                 alojamiento.setFechaOn(rs.getDate("Fecha_Salida").toLocalDate());
@@ -161,7 +158,7 @@ public class Alojamiento_Data {
         }
     }
     
-    // METODO PARA BUSCAR LOS ALOJAMIENTOS PASANDO UN OBJETO DEL TIPO CIUDAD (pero busca por el id de dicho objeto)    
+    // METODO PARA BUSCAR LOS ALOJAMIENTOS PASANDO UN OBJETO DEL TIPO CIUDAD (busca por el id de la ciudad)    
     public List<Alojamiento> alojamientoPorCiudad(Ciudad ciudad){
         
         ArrayList<Alojamiento> alojamientos = new ArrayList<>();
@@ -193,4 +190,38 @@ public class Alojamiento_Data {
         return alojamientos;
     }
     
+    // Prueba de metodo de busqueda por nombre ya sea pais, ciudad o provincia y armado del combo box
+//    public List<Ciudad> buscarCompleto(String txt){
+//        
+//        ArrayList<Ciudad> ciudades = new ArrayList<>();
+//        
+//        String sql = "SELECT * FROM ciudad WHERE Nombre LIKE '?%' OR "
+//                + "Provincia LIKE '?%' OR Pais LIKE '?%' ORDER BY `ciudad`.`Nombre`";
+//        
+//        try{
+//            PreparedStatement ps = con.prepareStatement(sql);
+//            ps.setString(1, txt);
+//            ps.setString(2, txt);
+//            ps.setString(3, txt);
+//            ResultSet rs = ps.executeQuery();
+//        if(rs.next()){
+//            ciudad.setIdCiudad(rs.getInt("idCiudad"));
+//            ciudad.setNombreCiudad(rs.getString("Nombre"));
+//            ciudad.setPais(rs.getString("Pais"));
+//            ciudad.setProvincia(rs.getString("Provincia"));
+//            ciudad.setEstado(rs.getBoolean("Estado"));
+//            ciudades.add(ciudad);
+//        }
+//        ps.close();
+//        
+////        for(Ciudad lista : ciudades){
+////            jcbPrueba.addItem(lista);
+////        }
+//        
+//        }catch(SQLException ex){
+//            JOptionPane.showMessageDialog(null, "Error al realizar la consulta en ComboBox "+ex);
+//        }
+//        return ciudades;
+//    } 
+
 }
--- Paquetes Turisticos/src/proyecto/transversal/vista/Main.java
+++ Paquetes Turisticos/src/proyecto/transversal/vista/Main.java
@@ -34,6 +34,7 @@ public class Main extends javax.swing.JFrame {
         jMenu1 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         jMenuItem1.setText("jMenuItem1");
 
@@ -63,6 +64,20 @@ public class Main extends javax.swing.JFrame {
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Edit");
+        jMenu2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenu2ActionPerformed(evt);
+            }
+        });
+
+        jMenuItem3.setText("jMenuItem3");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem3);
+
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
@@ -87,6 +102,17 @@ public class Main extends javax.swing.JFrame {
         jEscritorio.add(GA);
         jEscritorio.moveToFront(GA);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenu2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu2ActionPerformed
+     
+    }//GEN-LAST:event_jMenu2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+          Prueba_Combobox pc = new Prueba_Combobox();
+        pc.setVisible(true);
+        jEscritorio.add(pc);
+        jEscritorio.moveToFront(pc);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -130,5 +156,6 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     // End of variables declaration//GEN-END:variables
 }
