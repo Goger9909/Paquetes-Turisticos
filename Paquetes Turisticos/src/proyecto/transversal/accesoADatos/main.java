7a5a4bf1300a18655507d7ed72c2b1ea7041eecf
ferben007 <141643647+ferben007@users.noreply.github.com>
2023-10-10 10:01:18 -0300

    main

--- Paquetes Turisticos/src/proyecto/transversal/accesoADatos/main.java
+++ Paquetes Turisticos/src/proyecto/transversal/accesoADatos/main.java
@@ -6,6 +6,9 @@
 package proyecto.transversal.accesoADatos;
 
 import java.sql.Connection;
+import java.time.LocalDate;
+import proyecto.transversal.entidades.Alojamiento;
+import proyecto.transversal.entidades.Ciudad;
 
 /**
  *
@@ -14,5 +17,12 @@ import java.sql.Connection;
 public class main {
     public static void main(String[] arg){
         Connection a1=Conexion.getConexion();
+        Ciudad_Data cd = new Ciudad_Data();
+        Ciudad c = new Ciudad(18,"Argentina", "Merlo", "San Luis", true);
+//        cd.agregarCiudad(c);
+        Alojamiento a = new Alojamiento(LocalDate.now(), LocalDate.now(), "Hotel", "Con desayuno", true, 10000, c);
+        Alojamiento_Data ad = new Alojamiento_Data();
+        
+        ad.guardarAlojamiento(a);
     }
 }
