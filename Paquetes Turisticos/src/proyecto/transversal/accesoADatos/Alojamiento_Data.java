7e6cf82d7d33854b626d1b3f7bfedf840916f6b8
MatiV14 <matias993@gmail.com>
2023-10-25 13:19:12 -0300

    Aloja Data con array modificados

--- Paquetes Turisticos/src/proyecto/transversal/accesoADatos/Alojamiento_Data.java
+++ Paquetes Turisticos/src/proyecto/transversal/accesoADatos/Alojamiento_Data.java
@@ -7,7 +7,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javax.swing.JOptionPane;
 import proyecto.transversal.entidades.Alojamiento;
@@ -16,8 +18,7 @@ import proyecto.transversal.entidades.Ciudad;
 public class Alojamiento_Data {
 
     Connection con = null;
-    Alojamiento alojamiento = new Alojamiento();
-    Ciudad ciudad = new Ciudad();
+    
 
     public Alojamiento_Data() {
         con = Conexion.getConexion();
@@ -124,7 +125,9 @@ public class Alojamiento_Data {
 
     // BUSQUEDA POR ID DE ALOJAMIENTO - PUEDE QUE NECESITES BUSQUEDA de alojamiento PERO CON LA ID DE LA CIUDAD PARA EL PAQUETE, ASI QUE LOS DEJO MAS ABAJO  A ESOS METODOS JAJA
     public Alojamiento buscarAlojamientoPorId(int id) {
-
+        Alojamiento alojamiento = new Alojamiento();
+        Ciudad ciudad = new Ciudad();
+        
         String sql = "SELECT Fecha_Inicio, Fecha_Salida, Tipo_Alojamiento, Servicio, "
                 + "Importe_Diario, Ciudad_Destino, Estado FROM alojamiento WHERE idAlojamiento=?";
 
@@ -156,6 +159,8 @@ public class Alojamiento_Data {
     public List<Alojamiento> alojamientoPorCiudad(Ciudad ciudad) {
 
         ArrayList<Alojamiento> alojamientos = new ArrayList<>();
+        Alojamiento alojamiento = new Alojamiento();
+        
 
         String sql = "SELECT idAlojamiento, Fecha_Inicio, Fecha_Salida, Tipo_Alojamiento, Servicio, "
                 + "Importe_Diario, Estado FROM alojamiento WHERE Ciudad_Destino=?";
@@ -219,13 +224,15 @@ public class Alojamiento_Data {
     public List<Alojamiento> buscarAlojamientofecha(LocalDate fechain) {
         ArrayList<Alojamiento> alojamientos = new ArrayList<>();
         String sql = "SELECT idAlojamiento, Tipo_Alojamiento, Servicio, Importe_Diario,Fecha_Salida, "
-                + "Ciudad_Destino, Estado FROM alojamiento WHERE Fecha_Inicio = ?";
+                + "Ciudad_Destino, Estado FROM alojamiento WHERE Fecha_Inicio = '?'";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setDate(1, java.sql.Date.valueOf(fechain));
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
+                Alojamiento alojamiento = new Alojamiento();
                 Ciudad_Data cd = new Ciudad_Data();
+                Ciudad ciudad = new Ciudad();
                 alojamiento.setIdAlojamiento(rs.getInt("idAlojamiento"));
                 alojamiento.setFechaIn(fechain);
                 alojamiento.setFechaOn(rs.getDate("Fecha_Salida").toLocalDate());
@@ -244,4 +251,62 @@ public class Alojamiento_Data {
         return alojamientos;
     }
 
+    
+    public List<Alojamiento> buscarAlojamientoMes(LocalDate fechain) {
+        List<Alojamiento> alojamientos = new ArrayList<>();
+        String sql = "SELECT * FROM alojamiento WHERE Fecha_Inicio >= '?' AND Fecha_Inicio <= '?'";
+        
+        int dia = 31;
+        int mes = fechain.getMonthValue();
+        int anio = fechain.getYear();
+        LocalDate fechtot=LocalDate.of(dia, mes, anio);
+        System.out.println("fechatotal"+ fechtot);
+        
+        try {
+            
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setDate(1, Date.valueOf(fechain));
+            System.out.println("1:"+1);
+            ps.setDate(2, Date.valueOf(fechtot));
+            System.out.println("2: "+2);
+//            ps.setInt(2,fechain.getYear());
+//            System.out.println("2: "+fechain.getYear());
+//            ps.setInt(3, fechain.getMonthValue());
+//            System.out.println("3: "+fechain.getMonthValue());
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                JOptionPane.showMessageDialog(null, "Ejecuta consulta sql");
+                Ciudad_Data cd = new Ciudad_Data();
+                Alojamiento alojamiento = new Alojamiento();
+                Ciudad ciudad = new Ciudad();
+                alojamiento.setIdAlojamiento(rs.getInt("idAlojamiento"));
+                alojamiento.setFechaIn(rs.getDate("Fecha_Inicio").toLocalDate());
+                alojamiento.setFechaOn(rs.getDate("Fecha_Salida").toLocalDate());
+                alojamiento.setTipoAlojamiento(rs.getString("Tipo_Alojamiento"));
+                alojamiento.setServicio(rs.getString("Servicio"));
+                alojamiento.setImporteDiario(rs.getDouble("Importe_Diario"));
+                ciudad = cd.buscarCiudadPorID(rs.getInt("Ciudad_Destino"));
+                alojamiento.setCiudadDest(ciudad);
+                alojamiento.setEstado(rs.getBoolean("Estado"));
+                
+                
+            System.out.println("" + alojamiento.getIdAlojamiento() );
+            System.out.println("ID: " + alojamiento.getIdAlojamiento());
+            System.out.println("Fecha de inicio: " + alojamiento.getFechaIn());
+            System.out.println("Fecha de salida: " + alojamiento.getFechaOn());
+            System.out.println("Tipo de alojamiento: " + alojamiento.getTipoAlojamiento());
+            System.out.println("Servicio: " + alojamiento.getServicio());
+            JOptionPane.showMessageDialog(null, "Ejecuta pero no muestra");
+                
+                
+                alojamientos.add(alojamiento);  
+                
+            }
+ 
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la base de datos" + ex);
+        }
+        return alojamientos;
+    }
 }
